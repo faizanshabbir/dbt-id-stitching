1d37594283060902520eb1ae7d0c57c5bfe6445a
Faizan Shabbir <faizan@acorns.com>
2022-12-19 19:15:33 -0600

    process views as well as tables

--- macros/generate_information_schema.sql
+++ macros/generate_information_schema.sql
@@ -18,6 +18,22 @@
     {% endfor %}
 {% endif %}
 
+{% if execute %}
+    {% set columns = [] %}
+    {% for s in input_schemas %}
+       {% set results = run_query('SHOW VIEWS IN ' + s) %}
+       {% for t in results.columns[1].values() %}
+            {% set col_result = run_query('SHOW COLUMNS IN '+ s + '.' + t) %}
+            {% do columns.append((s, t, col_result.columns[0].values())) %}
+        {% endfor %}
+    {% endfor %}
+    {% for res in columns %}
+        {% for x in res[2] %}
+            {% do final_col_list.append((x, res[0], res[1])) %}
+        {% endfor %}
+    {% endfor %}
+{% endif %}
+
 SELECT
 {% for item in final_col_list%}
 '{{item[0]}}' AS column_name,
